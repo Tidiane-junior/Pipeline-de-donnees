cce85146065636711d410402a85cfd4457b17b38
Tidiane-junior <77800865+Tidiane-junior@users.noreply.github.com>
2026-01-29 17:12:12 +0100

    Ingestion des donnees

--- data_engineer_day2/src/main.py
+++ data_engineer_day2/src/main.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 #%% Utiser la fonction "load_csv" de ingestion.py pour charger les fichiers CSV
 from ingestion import load_csv
 
 #%% Fonction principale
 def main():
-    print("🚀 Debut du pipeline – Etape ingestion")
+    print("Debut du pipeline – Etape ingestion")
 
     path_orders = "D:\\Data_Engineer_Bootcamp\\Pipeline-de-donnees\\data_engineer_day2\\data\\raw\\orders.csv"
     path_products = "D:\\Data_Engineer_Bootcamp\\Pipeline-de-donnees\\data_engineer_day2\\data\\raw\\products.csv"
@@ -13,7 +14,7 @@ def main():
     products = load_csv(path_products)
     customers = load_csv(path_customers)
 
-    print("✅ Fichiers chargés avec succès")
+    print("Fichiers chargés avec succès")
     print(f"Orders: {orders.shape}")
     print(f"Products: {products.shape}")
     print(f"Customers: {customers.shape}")
